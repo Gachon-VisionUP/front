e92f7d0b501f7003d2c8d0a4d848929925e4a31e
jodan <wdd789q@naver.com>
2025-01-08 03:12:44 +0900

    feat : 바텀네비게이션 UI 대폭 수정

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,14 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
-
+import { Platform, Image } from 'react-native';
 import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import graph from '@/assets/images/navigation/graph.png';
+import paper from '@/assets/images/navigation/paper.png';
+import pencil from '@/assets/images/navigation/pencil.png';
+import home from '@/assets/images/navigation/home.png';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -14,44 +16,87 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: '#F58220',
+        tabBarInactiveTintColor: '#000',
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
+        tabBarStyle: {
+          backgroundColor: '#fff',
+          height: 60,
+          borderTopWidth: 0,
+          position: Platform.select({
+            ios: 'absolute',
+            default: 'relative',
+          }),
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+        },
+      }}
+    >
       <Tabs.Screen
         name="home"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: '홈',
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={home}
+              style={{
+                width: 24,
+                height: 24,
+                tintColor: focused ? '#F58220' : '#000',
+              }}
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="exp"
         options={{
-          title: 'Exp',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: '경험치 현황',
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={graph}
+              style={{
+                width: 24,
+                height: 24,
+                tintColor: focused ? '#F58220' : '#000',
+              }}
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="quest"
         options={{
-          title: 'Quest',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: '퀘스트 현황',
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={paper}
+              style={{
+                width: 24,
+                height: 24,
+                tintColor: focused ? '#F58220' : '#000',
+              }}
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="board"
         options={{
-          title: 'Board',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: '게시판',
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={pencil}
+              style={{
+                width: 24,
+                height: 24,
+                tintColor: focused ? '#F58220' : '#000',
+              }}
+            />
+          ),
         }}
       />
     </Tabs>
