38a73ca918ada982beb2ff21dd90b2fe4ba18bc7
jodan <wdd789q@naver.com>
2025-01-07 15:26:21 +0900

    style : 로그인 버튼 UI 개발

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -2,7 +2,7 @@ import { Link, Stack } from "expo-router";
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 import img from "../../assets/images/login/Title.png";
-import styled, { css } from 'styled-components/native';
+import styled from 'styled-components/native';
 import Input from "../../components/login/input";
 
 export default function LoginPage() {
@@ -16,14 +16,13 @@ export default function LoginPage() {
           style={styles.imageStyle}
         />
         <Input />
-        <Link
-          style={styles.button}
-          href={{
-            pathname: "/home",
-          }}
+        <LoginButton
+          onPress={() => console.log("로그인 버튼 클릭")} // 로그인 버튼 클릭 시 처리할 함수
+          as={Link}
+          href="/home"
         >
-          <Text style={styles.buttonText}>홈으로</Text>
-        </Link>
+          <ButtonText>로그인</ButtonText>
+        </LoginButton>
       </View>
     </>
   );
@@ -35,27 +34,25 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-    borderColor: "red",
-  },
-  button: {
-    marginTop: 20,
-    padding: 20,
-    backgroundColor: "skyblue",
-    borderRadius: 10,
-  },
-  buttonText: {
-    color: "white",
-    fontSize: 16,
   },
   imageStyle: {
-    width: "50%",                     //가로비율
-    height: "7%",                    //세로비율
-    alignItems: "center",              //중간정렬
-    justifyContent: "center"
-  }
+    width: "50%", //가로비율
+    height: "5%", //세로비율
+    alignItems: "center", //중간정렬
+    justifyContent: "center",
+  },
 });
 
-const StyledButton = styled.Image`
-  margin: 30px;
-  background-color: blue;
+const LoginButton = styled.TouchableOpacity`
+  background-color: #C4C4C4;
+  padding: 20px;
+  align-items: center;
+  justify-content: center;
+  width: 90%;
+`;
+
+const ButtonText = styled.Text`
+  color: white;
+  font-size: 16px;
+  text-align: center;
 `;
